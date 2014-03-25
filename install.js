4c15d87f42e7f45f9d8a4334c87299683f2e8238
brutalhonesty <sparky1010@gmail.com>
2014-03-25 01:58:25 -0700

    Updated express back to session inside configure, add install script for creating upload folder

---
+++ install.js
@@ -0,0 +1,9 @@
+var config = require('./config/config');
+var colors = require('colors');
+var fs = require('fs');
+fs.mkdir(path.join(config.root, 'uploads'), function (err) {
+  if(err) {
+    throw err;
+  }
+  console.log('Upload directory created!'.green);
+});
