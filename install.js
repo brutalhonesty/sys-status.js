2c972a6cd5cd7c3ed9c42fb3c75f05965a6f9075
brutalhonesty <sparky1010@gmail.com>
2014-03-25 02:19:05 -0700

    Switched to mkdirp for folder creation

--- install.js
+++ install.js
@@ -1,10 +1,32 @@
 var config = require('./lib/config/config');
 var colors = require('colors');
 var path = require('path');
-var fs = require('fs');
-fs.mkdir(path.join(config.root, 'uploads'), function (err) {
+var mkdirp = require('mkdirp');
+mkdirp(path.join(config.root, 'uploads'), 0755, function (err) {
   if(err) {
-    throw err;
+    console.log(err.red);
+    return;
   }
   console.log('Upload directory created!'.green);
+  mkdirp(path.join(config.root, 'uploads', 'cover'), 0755, function (err) {
+    if(err) {
+      console.log(err.red);
+      return;
+    }
+    console.log('Cover directory created!'.green);
+    mkdirp(path.join(config.root, 'uploads', 'logo'), 0755, function (err) {
+      if(err) {
+        console.log(err.red);
+        return;
+      }
+      console.log('Logo directory created!'.green);
+      mkdirp(path.join(config.root, 'uploads', 'favicon'), 0755, function (err) {
+        if(err) {
+          console.log(err.red);
+          return;
+        }
+        console.log('Favicon directory created!'.green);
+      });
+    });
+  });
 });
