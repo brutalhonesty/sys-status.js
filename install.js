9aef0d6eec9731c12475e13536d192bf8dd5f028
brutalhonesty <sparky1010@gmail.com>
2014-03-25 02:02:31 -0700

    Added installs script to npm, edit install script to right path

--- install.js
+++ install.js
@@ -1,4 +1,4 @@
-var config = require('./config/config');
+var config = require('./llib/config/config');
 var colors = require('colors');
 var fs = require('fs');
 fs.mkdir(path.join(config.root, 'uploads'), function (err) {
