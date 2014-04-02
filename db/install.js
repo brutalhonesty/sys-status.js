1961c35e455edcfb6b778d2bb4e4dbd91c8bf60a
brutalhonesty <sparky1010@gmail.com>
2014-04-01 17:04:00 -0700

    Added sites view to DB install file

--- db/install.js
+++ db/install.js
@@ -2,6 +2,7 @@ var settings = require('../lib/controllers/settings');
 var nano = require('nano')(settings.couchdb.url);
 // TODO Move this DB view and the one in the api.js mock file to a setting file somewhere.
 var userView = {views: {"all": {"map": "function(doc) {emit(null, doc) }","reduce": "_count"},"by_email": {"map": "function(doc) {emit(doc.email, doc) }","reduce": "_count"},"by_user": {"map": "function(doc) {emit(doc._id, doc) }","reduce": "_count"}}};
+var sitesView = {"views":{"all":{"map":"function(doc) {emit(null, doc) }","reduce":"_count"},"by_url":{"map":"function(doc) {emit(doc.domain, doc) }","reduce":"_count"},"by_id":{"map":"function(doc) {emit(doc._id, doc) }","reduce":"_count"}}};
 var colors = require('colors');
 nano.db.create(settings.couchdb.users, function (err, body) {
   if(err && err.status_code !== 412) {
@@ -23,6 +24,12 @@ nano.db.create(settings.couchdb.users, function (err, body) {
       nano.db.create(settings.couchdb.sites, function (err) {
         if(err && err.status_code !== 412) {
           console.log(err);
+          return;
+        }
+        var sites = nano.db.use(settings.couchdb.sites);
+        sites.insert(sitesView, '_design/sites', function (err) {
+        if(err) {
+          console.log('Error recreating database.'.red);
           return;
         }
         console.log('DB Installation successful.'.green);
